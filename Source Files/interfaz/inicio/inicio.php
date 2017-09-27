c366352c4c615900287376752d9d1fa9df8736f6
barrutieta <barrutieta@10.22.72.121>
2017-09-27 10:42:25 +0200

    esquema de carpetas

--- Source Files/interfaz/inicio/inicio.php
+++ Source Files/interfaz/inicio/inicio.php
@@ -1,8 +1,27 @@
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Mobile first -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <header>
+            <div class="container-fluid">
+                <h1>EKOthazi</h1>
+                <div class="row">
+                    <div class="col-sm-4" style="background-color:lavender;">Inicio</div>
+                    <div class="col-sm-4" style="background-color:lavender;">Quienes somos</div>
+                    <div class="col-sm-4" style="background-color:lavender;">Noticias</div>
+                    <div class="col-sm-4" style="background-color:lavender;">Productos</div>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+?>
