ebd04a315c2c919407bd698ae82e59b76c027feb
ikaslea <ikaslea@10.22.72.249>
2017-09-27 12:28:40 +0200

    Sección productos en mantenimiento. Falta estructura

--- Source Files/interfaz/productos/productos.php
+++ Source Files/interfaz/productos/productos.php
@@ -1,8 +1,25 @@
-<?php
+<html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<head>
+<title>Ekothazi</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<link rel="stylesheet" href="../../css/productos.css">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+</head>
+<body>
+	<div class="imagenMantenimiento">
+		<img src="../../img/Lawn-Maintenance-Icon.png" alt="" />
+
+		<h1 class="h1Mantenimiento">Oops!</h1>
+		<p class="textoMantenimiento">La página no está disponible por
+			mantenimiento.</p>
+		<p class="textoMantenimiento">Volvemos en breve!</p>
+	</div>
+</body>
+</html>
