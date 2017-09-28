2442cd52bfba70ccb648ef8b6103fd0073bba768
barrutieta <barrutieta@10.22.72.121>
2017-09-28 09:47:27 +0200

    header puesto en inicio

--- Source Files/interfaz/inicio/inicio.php
+++ Source Files/interfaz/inicio/inicio.php
@@ -11,52 +11,94 @@
 	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<!-- registro con boostrap -->
-	<div class="input-group">
-		<span class="input-group-addon"></span>
+	<header>
 		<div class="container">
-			<h2>INICIO DE SESION</h2>
-			<form action="registrar.php">
-				<div class="form-group">
-					<label for="email">Email:</label> <input type="email"
-						class="form-control" id="email" placeholder="email" name="email">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div background-color="green" class="navbar-header">
+						<img src="C:\Users\barrutieta\Desktop\logo.png"
+							style="height: 150px;">
+					</div>
+					<div class="container-fluid">
+
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<button type="button" class="btn btn-link" data-toggle="modal"
+									data-target="#myModal">
+									<span class="glyphicon glyphicon-log-in"></span> Iniciar sesion
+								</button>
+							</li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right" style="margin-top: 75px;">
+							<li class="active"><a href="#">INICIO</a></li>
+							<li class="dropdown"><a class="dropdown-toggle"
+								data-toggle="dropdown" href="#">QUIENES SOMOS <span
+									class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Donde encontrarnos</a></li>
+									<li><a href="#">Nuestros proyectos</a></li>
+									<li><a href="#">Politica de empresa</a></li>
+									<li><a href="#">Equipo humano</a></li>
+								</ul></li>
+							<li><a href="#">NOTICIAS</a></li>
+							<li><a href="#">PRODUCTOS</a></li>
+						</ul>
+					</div>
+					<div class="modal fade" id="myModal" role="dialog">
+						<div class="modal-dialog">
+
+							<!-- Modal content-->
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal">&times;</button>
+									<h4 class="modal-title">Inicio de sesion</h4>
+								</div>
+								<div class="modal-body">
+									<form>
+										<div class="form-group">
+											<label for="email">Direccion de correo electronico:</label> <input
+												type="email" class="form-control" id="email">
+										</div>
+										<div class="form-group">
+											<label for="pwd">Contraseña:</label> <input type="password"
+												class="form-control" id="pwd">
+										</div>
+										<div class="checkbox">
+											<label><input type="checkbox"> Recordarme en esta pagina</label>
+										</div>
+										<button type="submit" class="btn btn-default">Enviar</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class="form-group">
-					<label for="pwd">Password:</label> <input type="password"
-						class="form-control" id="pwd" placeholder="Contraseña" name="pwd">
-				</div>
-				<div class="checkbox">
-					<label><input type="checkbox" name="remember"> Remember me</label>
-				</div>
-				<button type="submit" class="btn btn-default" name="enviar"
-					value="enviar">Submit</button>
-			</form>
+			</nav>
 		</div>
-	</div>
-	<!-- twiter -->
-	<div class="row">
-		<div class="col-md-3">
-			<div style="position: fixed; left: 79.1%;">
-				<a class="twitter-timeline"
-					href="https://twitter.com/hashtag/ecologico"
-					data-widget-id="912648408506257408">Tweets sobre #ecologico</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8">aaa</div>
+			<div class="col-sm-4">
+				<div class="thumbnail">
+					<a href="../productos/productos.php" target="_blank"> <img
+						src="../../img/ecomarket.png" style="width: 50%">
+					</a>
+				</div>
+				<div class="row">
+						<div>
+							<a class="twitter-timeline"
+								href="https://twitter.com/hashtag/ecologico"
+								data-widget-id="912648408506257408">Tweets sobre #ecologico</a>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],
 p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+
 "://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 </script>
+						</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<!-- imagen -->
-	<div class="row">
-		<div class="col-md-4">
-			<div class="thumbnail">
-				<a href="../productos/productos.php" target="_blank"> <img
-					src="../../img/ecomarket.png" style="width: 50%">
-					<div class="caption">
-						<p></p>
-					</div>
-				</a>
+		<!-- twiter -->
 
 </body>
 </html>
