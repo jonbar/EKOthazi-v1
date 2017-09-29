62f7aca0941ceb3b2f20f935840fe963f5728377
ikaslea <ikaslea@10.22.72.15>
2017-09-29 14:49:44 +0200

    Noticias

--- Source Files/interfaz/noticias/noticias.php
+++ Source Files/interfaz/noticias/noticias.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="../../css/noticias.css">
 <link rel="stylesheet" href="../../css/inicio.css">
+<link rel="stylesheet" href="../../css/general.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 
@@ -14,10 +15,14 @@
 
 
 </header>	
+<div class="container-fluid">
+<div class="col-sm-12">
    <?php     
         
         $link = mysqli_connect('localhost', 'root', '', 'ekothazi');                      
         $sql="SELECT u.nombre,e.titulo,e.cuerpo,e.fecha FROM usuarios u, entrada_blog e WHERE u.id=e.id_autor";
+        mysqli_set_charset($link, "utf8"); /* Procedural approach */
+        $link->set_charset("utf8"); 
         $result=mysqli_query($link, $sql);
         
         
@@ -37,32 +42,52 @@
         mysqli_free_result($result);
                 
     ?>  
-   
-<footer class="w3-container w3-light-grey w3-padding-32 w3-margin-top">
-  <div class="row" style="background-color: #E6E6E6;">
+    </div>
+   </div>
+	<footer>
+			<div class="container-fluid">
+				<div class="row" style="background-color: #E6E6E6;">
 					<div class="col-sm-4">
 						<img src="../../img/logoLargoFooter.png" class="imagenFooter">
 					</div>
 					<div class="col-sm-3">
-					<h4 style="color: 7ac043;">Informacion</h4>
-					<ul style="color: 7ac043">
-						<li>Aviso legal</li>
-						<li>Preguntas frecuentes</li>						
-						<li>Trabaja con nosotros</li>
-						<li>Envios y devoluciones</li>
-					</ul>
+						<h4 style="color: 7ac043;">Informacion</h4>
+						<ul style="color: 7ac043">
+							<li>Aviso legal</li>
+							<li>Preguntas frecuentes</li>
+							<li>Trabaja con nosotros</li>
+							<li>Envios y devoluciones</li>
+						</ul>
 					</div>
+
 					<div class="col-sm-3">
-					<h4 style="color: 7ac043">Visita nuestro blog</h4>
-					<ul>
-						<li style="color: 7ac043">Blog</li>
-					</ul>
+						<h4 style="color: 7ac043">Visita nuestro blog</h4>
+						<ul>
+							<li style="color: 7ac043">Blog</li>
+						</ul>
 					</div>
-</div>
-				<div class="row derechos" style="background-color: #E6E6E6;">
-					&#169; EKOthazi S.L. 2005-2017. TODOS LOS DERECHOS RESERVADOS
+					<div class="col-sm-2">
+						<table style="margin-top: 15px">
+							<tr>
+								<td colspan="2" style="text-align: center"><a href="#"
+									data-toggle="tooltip" data-placement="bottom" title="Facebook"><img
+										src="../../img/facebook_icon.png" class="redSocialFooter" /></a></td>
+							</tr>
+							<tr>
+								<td><a href="#" data-toggle="tooltip" data-placement="bottom"
+									title="Twitter"><img src="../../img/twitter_icon.png"
+										class="redSocialFooter" /></a></td>
+								<td><a href="#" data-toggle="tooltip" data-placement="bottom"
+									title="Instagram"><img src="../../img/instagram_icon.png"
+										class="redSocialFooter" /></a></td>
+							</tr>
+						</table>
+					</div>
 				</div>
-</footer>       
+				<div class="row derechos" style="background-color: #E6E6E6;">&#169;
+					EKOthazi S.L. 2005-2017. TODOS LOS DERECHOS RESERVADOS</div>
+			</div>
+		</footer>      
 
 </body>
 </html>
