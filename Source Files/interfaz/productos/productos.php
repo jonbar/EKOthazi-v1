274ff151c8ca657019f55dc5950795bb1588f687
ikaslea <ikaslea@10.22.72.204>
2017-10-04 11:05:21 +0200

    Todos los links funcionando

--- Source Files/interfaz/productos/productos.php
+++ Source Files/interfaz/productos/productos.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-<title>Ekothazi</title>
+<title>Productos</title>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 
 <link rel="stylesheet" href="../../css/productos.css">
@@ -26,7 +26,7 @@
 			<nav class="navbar navbar-default">
 				<div class="container-fluid">
 					<div background-color="green" class="navbar-header">
-						<img src="../../img/logo largo.png" style="height: 150px;">
+						<a href="../inicio/inicio.php"><img src="../../img/logo largo.png" style="height: 150px;"></a>
 					</div>
 					<div class="container-fluid">
 
@@ -34,13 +34,13 @@
 							<li>
 								<button type="button" class="btn btn-link" data-toggle="modal"
 									data-target="#myModal">
-									<span class="glyphicon glyphicon-log-in"></span> Iniciar sesion
+									<span class="glyphicon glyphicon-log-in"></span> Iniciar sesión
 								</button>
 							</li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right" style="margin-top: 75px;">
 							<li><a href="../inicio/inicio.php">INICIO</a></li>
-							<li><a href="../quienes_somos/quienes_somos.php">QUIENES SOMOS </a></li>
+							<li><a href="../quienes_somos/quienes_somos.php">QUIENES SOMOS</a></li>
 							<li><a href="../noticias/noticias.php">NOTICIAS</a></li>
 							<li class="active"><a href="#">PRODUCTOS</a></li>
 						</ul>
@@ -52,20 +52,20 @@
 							<div class="modal-content">
 								<div class="modal-header">
 									<button type="button" class="close" data-dismiss="modal">&times;</button>
-									<h4 class="modal-title">Inicio de sesion</h4>
+									<h4 class="modal-title">Inicio de sesión</h4>
 								</div>
 								<div class="modal-body">
 									<form>
 										<div class="form-group">
-											<label for="email">Direccion de correo electronico:</label> <input
+											<label for="email">Dirección de correo electrónico:</label> <input
 												type="email" class="form-control" id="email">
 										</div>
 										<div class="form-group">
-											<label for="pwd">Contrase�a:</label> <input type="password"
+											<label for="pwd">Contraseña:</label> <input type="password"
 												class="form-control" id="pwd">
 										</div>
 										<div class="checkbox">
-											<label><input type="checkbox"> Recordarme en esta pagina</label>
+											<label><input type="checkbox"> Recordarme en esta página</label>
 										</div>
 										<button type="submit" class="btn btn-default">Enviar</button>
 									</form>
@@ -77,14 +77,13 @@
 			</nav>
 		</div>
 	</header>
-	<div class="container">
+	<div class="container mantenimientoProductos" >
 	<div class="imagenMantenimiento">
 		<img src="../../img/maintenanceicon.png" alt="" />
 
-		<h1 class="h1Mantenimiento">Oops!</h1>
-		<p class="textoMantenimiento">La p&#225;gina no est&#225; disponible por
-			mantenimiento.</p>
-		<p class="textoMantenimiento">Volvemos en breve!</p>
+		<h2 class="h1Mantenimiento">Oops... ¡Página en mantenimiento!</h2>
+		<p class="textoMantenimiento">Disculpen las molestias, estamos trabajando</br>en esta sección para que pronto podáis disfrutarla.</p>
+		<p class="textoMantenimiento">¡Volvemos en breve!</p>
 	</div>
 	</div>
 	<footer>
@@ -105,23 +104,25 @@
 				</div>
 
 				<div class="col-sm-3">
-					<h4 style="color: 7ac043">Visita nuestro blog</h4>
+					<h4 style="color: 7ac043">Conócenos</h4>
 					<ul>
-						<li style="color: 7ac043">Blog</li>
+						
+						<a href="../quienes_somos/quienes_somos.php" style="color: 7ac043"><li>Contacto</a></li>
+						<a href="../noticias/noticias.php" style="color: 7ac043"><li>Blog</a></li>
 					</ul>
 				</div>
 				<div class="col-sm-2">
 					<table style="margin-top: 15px">
 						<tr>
-							<td colspan="2" style="text-align: center"><a href="#"
+							<td colspan="2" style="text-align: center"><a href="https://www.facebook.com/ecotiendaorganicaynatural/"
 								data-toggle="tooltip" data-placement="bottom" title="Facebook"><img
 									src="../../img/facebook_icon.png" class="redSocialFooter" /></a></td>
 						</tr>
 						<tr>
-							<td><a href="#" data-toggle="tooltip" data-placement="bottom"
+							<td><a href="https://twitter.com/Ecotiendagdl" data-toggle="tooltip" data-placement="bottom"
 								title="Twitter"><img src="../../img/twitter_icon.png"
 									class="redSocialFooter" /></a></td>
-							<td><a href="#" data-toggle="tooltip" data-placement="bottom"
+							<td><a href="https://web.stagram.com/ecotiendaorganica" data-toggle="tooltip" data-placement="bottom"
 								title="Instagram"><img src="../../img/instagram_icon.png"
 									class="redSocialFooter" /></a></td>
 						</tr>
