834b63951f4c672c7d8497b1bbd7d73a6a1e8771
barrutieta <barrutieta@10.22.72.121>
2017-10-06 13:02:37 +0200

    login

--- Source Files/interfaz/inicio/inicio.php
+++ Source Files/interfaz/inicio/inicio.php
@@ -3,10 +3,7 @@
 <head>
 <?php
 session_start();
-$link = mysqli_connect('localhost', 'root', '', 'ekothazi'); 
-if (isset($_SESSION['email'])) {
-    echo '<script>alert("ya esta")  </script>';
-}
+
 ?>
 <title>Inicio</title>
 <meta charset="utf-8">
@@ -15,8 +12,7 @@ if (isset($_SESSION['email'])) {
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script
 	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script
-	src="../../javascript/main.js"></script>
+<script src="../../javascript/main.js"></script>
 <script
 	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="../../css/inicio.css"
@@ -26,23 +22,32 @@ if (isset($_SESSION['email'])) {
 
 </head>
 <body style="text-align: justify;">
+
 	<header>
 		<div class="container">
 			<nav class="navbar navbar-default">
 				<div class="container-fluid">
 					<div background-color="green" class="navbar-header">
 						<a href="../inicio/inicio.php"><img src="../../img/logo largo.png"
-							style="height:25%"></a>
+							style="height: 25%"></a>
 					</div>
 					<div class="container-fluid">
-
 						<ul class="nav navbar-nav navbar-right">
-							<li>
+						<?php
+                            if ($_SESSION == true) {
+                                echo 'Bienvenido ' . $_SESSION['username'];
+                            } else {
+                                ?>
+                                <li>
 								<button type="button" class="btn btn-link" data-toggle="modal"
 									data-target="#myModal">
 									<span class="glyphicon glyphicon-log-in"></span> Iniciar sesion
 								</button>
 							</li>
+                                <?php 
+                            }
+                           ?>
+							
 						</ul>
 						<ul class="nav navbar-nav navbar-right" style="margin-top: 75px;">
 							<li class="active"><a href="#">INICIO</a></li>
@@ -57,6 +62,7 @@ if (isset($_SESSION['email'])) {
 							<!-- Modal content-->
 							<div class="modal-content">
 								<div class="modal-header">
+
 									<button type="button" class="close" data-dismiss="modal">&times;</button>
 									<h4 class="modal-title">Inicio de sesión</h4>
 								</div>
@@ -66,11 +72,12 @@ if (isset($_SESSION['email'])) {
 										<div class="form-group">
 											<label for="input">Cuenta de correo </label> <input
 												type="text" name="correo" id="correo"
-												onKeyPress="return comprobarArroba(event)" />
+												onKeyPress="return comprobarArroba(event)"
+												required="required" />
 										</div>
 										<div class="form-group">
 											<label for="input">Introduzca clave de acceso </label><input
-												type="password" id="clave" name="clave"/>
+												type="password" id="clave" name="clave" required="required" />
 										</div>
 										<div class="checkbox">
 											<label><input type="checkbox"> Recordarme en esta página</label>
@@ -94,7 +101,7 @@ if (isset($_SESSION['email'])) {
     
     $sql = "SELECT * FROM `entrada_blog` WHERE id_entrada = ( SELECT MAX(`id_entrada`) FROM entrada_blog)";
     mysqli_set_charset($link, "utf8"); /* Procedural approach */
-
+    
     $result = mysqli_query($link, $sql);
     
     $row = mysqli_fetch_array($result);
@@ -110,9 +117,9 @@ if (isset($_SESSION['email'])) {
 			<div class="col-sm-4">
 				<div class="thumbnail">
 					<button type="button" class="btn" data-toggle="modal"
-						data-target="#myModalRegistro" style="width: 350px">
+						data-target="#myModalRegistro" style="width: 100%">
 						<span class="glyphicons glyphicons-leaf"></span><b>Regístrate en
-						nuestra página</b>
+							nuestra página</b>
 					</button>
 					<div class="modal fade" id="myModalRegistro" role="dialog">
 						<div class="modal-dialog">
@@ -124,18 +131,17 @@ if (isset($_SESSION['email'])) {
 									<h4 class="modal-title">Registro</h4>
 								</div>
 								<div class="modal-body">
-									<!-- 								e tenido que quitar la parte de boostrap en los inputs -->
-									<form name="registro" action="inicio.php"
-										onsubmit="return enviarRegistro()" method="POST">
+									<!-- e tenido que quitar la parte de boostrap en los inputs -->
+									<form name="encuestaRegistro" action="validacion.php"
+										method="POST" onsubmit="return enviar()">
 										<div class="form-group">
 											<label for="input">Cuenta de correo </label> <input
-												type="text" name="correoRegistro"
-												onKeyPress="return comprobarArrobaRegistro(event)" />
+												type="text" name="correo" id="correo"
+												onKeyPress="return comprobarArrobaa(event)" />
 										</div>
 										<div class="form-group">
 											<label for="input">Introduzca clave de acceso </label><input
-												type="password" id="clave1" /> <label for="input">Repita
-												clave de acceso </label><input type="password" id="clave2" />
+												type="password" id="clave" name="clave" />
 										</div>
 										<div class="checkbox">
 											<label><input type="checkbox"> Recordarme en esta página</label>
@@ -164,12 +170,12 @@ if (isset($_SESSION['email'])) {
 						<td><a href="#" data-toggle="tooltip" data-placement="bottom"
 							title="Facebook"><img src="../../img/facebook_icon.png"
 								class="redSocial" /></a></td>
-						<td><a href="https://twitter.com/Ecotiendagdl" data-toggle="tooltip" data-placement="bottom"
-							title="Twitter"><img src="../../img/twitter_icon.png"
-								class="redSocial" /></a></td>
-						<td><a href="https://web.stagram.com/ecotiendaorganica" data-toggle="tooltip" data-placement="bottom"
-							title="Instagram"><img src="../../img/instagram_icon.png"
-								class="redSocial" /></a></td>
+						<td><a href="https://twitter.com/Ecotiendagdl"
+							data-toggle="tooltip" data-placement="bottom" title="Twitter"><img
+								src="../../img/twitter_icon.png" class="redSocial" /></a></td>
+						<td><a href="https://web.stagram.com/ecotiendaorganica"
+							data-toggle="tooltip" data-placement="bottom" title="Instagram"><img
+								src="../../img/instagram_icon.png" class="redSocial" /></a></td>
 					</table>
 				</div>
 				<div class="row">
@@ -193,38 +199,42 @@ p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.create
 					</div>
 					<div class="col-sm-3">
 						<h4 style="color: 7ac043;">Información</h4>
-					<ul style="color: 7ac043">
-						<li>Aviso legal</li>
-						<li>Preguntas frecuentes</li>
-						<li>Trabaja con nosotros</li>
-						<li>Envíos y devoluciones</li>
-					</ul>
-				</div>
+						<ul style="color: 7ac043">
+							<li>Aviso legal</li>
+							<li>Preguntas frecuentes</li>
+							<li>Trabaja con nosotros</li>
+							<li>Envíos y devoluciones</li>
+						</ul>
+					</div>
 
-				<div class="col-sm-3">
-					<h4 style="color: 7ac043">Conócenos</h4>
-					<ul>
-						
-						<a href="../quienes_somos/quienes_somos.php" style="color: 7ac043"><li>Contacto</a></li>
-						<a href="../noticias/noticias.php" style="color: 7ac043"><li>Blog</a></li>
-					</ul>
-				</div>
-				<div class="col-sm-2">
-					<table style="margin-top: 15px">
-						<tr>
-							<td colspan="2" style="text-align: center"><a href="https://www.facebook.com/ecotiendaorganicaynatural/"
-								data-toggle="tooltip" data-placement="bottom" title="Facebook"><img
-									src="../../img/facebook_icon.png" class="redSocialFooter" /></a></td>
-						</tr>
-						<tr>
-							<td><a href="https://twitter.com/Ecotiendagdl" data-toggle="tooltip" data-placement="bottom"
-								title="Twitter"><img src="../../img/twitter_icon.png"
-									class="redSocialFooter" /></a></td>
-							<td><a href="https://web.stagram.com/ecotiendaorganica" data-toggle="tooltip" data-placement="bottom"
-								title="Instagram"><img src="../../img/instagram_icon.png"
-									class="redSocialFooter" /></a></td>
-						</tr>
-					</table>
+					<div class="col-sm-3">
+						<h4 style="color: 7ac043">Conócenos</h4>
+						<ul>
+
+							<a href="../quienes_somos/quienes_somos.php"
+								style="color: 7ac043"><li>Contacto</a>
+							</li>
+							<a href="../noticias/noticias.php" style="color: 7ac043"><li>Blog</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-sm-2">
+						<table style="margin-top: 15px">
+							<tr>
+								<td colspan="2" style="text-align: center"><a
+									href="https://www.facebook.com/ecotiendaorganicaynatural/"
+									data-toggle="tooltip" data-placement="bottom" title="Facebook"><img
+										src="../../img/facebook_icon.png" class="redSocialFooter" /></a></td>
+							</tr>
+							<tr>
+								<td><a href="https://twitter.com/Ecotiendagdl"
+									data-toggle="tooltip" data-placement="bottom" title="Twitter"><img
+										src="../../img/twitter_icon.png" class="redSocialFooter" /></a></td>
+								<td><a href="https://web.stagram.com/ecotiendaorganica"
+									data-toggle="tooltip" data-placement="bottom" title="Instagram"><img
+										src="../../img/instagram_icon.png" class="redSocialFooter" /></a></td>
+							</tr>
+						</table>
 					</div>
 				</div>
 				<div class="row derechos" style="background-color: #E6E6E6;">&#169;
--- Source Files/javascript/main.js
+++ Source Files/javascript/main.js
@@ -19,65 +19,72 @@ function validarEmail() {
 }
 
 
+function Enviar() {
+
+    if (Correo() && Clave()){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function Clave(){
+    var clav1=document.getElementById('clave1');
+    var clav2=document.getElementById('clave2');
+
+    if ((clav1.value=="")||(clav1.value!=clav2.value)){
+        alert('Claves incorrectas');
+        return false;
+    }else {
+        return true;
+    }
+}
+
+function Correo() {
+      var miCorreo=document.encuesta.correo.value;
+      if (/\w+@\w+.\w+/.test(miCorreo)){
+           return true;
+      } else {
+       alert("La dirección de email es incorrecta.");
+       return false;
+     }
+}
+function teclaCP(e){
+	var keychar;
+	var teclanum;
+	var codigoPostal = document.getElementById('codPostal').value;
+	var longitud = codigoPostal.length;
+	
+	if(longitud => 5){
+		return false;
+	}
+	
+	teclanum = e.which;
+	keychar = String.fromCharCode(teclanum);
+	
+	if (keychar < '0' || keychar > '9'){
+		return false;
+	}else{
+		return true;
+	}
+}
 function comprobarArroba(e) {
-	var caracterTecla;
-	var codigoTecla;
+    var caracterTecla;
+    var codigoTecla;
 
-	codigoTecla = e.which;
+    if (window.event){ //IE8 and earlier
+        codigoTecla = e.keyCode;
+    }else if (e.which){ // IE9/Firefox/Chrome/Opera/Safari
+        codigoTecla = e.which;
+        }
+    
+    caracterTecla = String.fromCharCode(codigoTecla);
 
-	caracterTecla = String.fromCharCode(codigoTecla);
-	if (caracterTecla == "@") {
-		if (document.encuesta.correo.value.indexOf('@') >= 0) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-}
-// parte de registro
-function enviarRegistro() {
-
-	if (validarEmailRegistro() && validarPassword()) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function validarEmailRegistro() {
-	var email = document.registro.correoRegistro.value;
-	if (/\w+@\w+.\w+/.test(email)) {
-		// alert("La dirección de email " + email + " es correcta.");
-		return true;
-	} else {
-		alert("La dirección de email es incorrecta.");
-		return false;
-	}
-}
-function validarPassword() {
-	var clave1 = document.getElementById('clave1');
-	var clave2 = document.getElementById('clave2');
-
-	if ((clave1.value == "") || (clave1.value != clave2.value)) {
-		alert('Las claves no coinciden');
-		return false;
-	} else {
-		return true;
-	}
-}
-function comprobarArrobaRegistro(e) {
-	var caracterTecla;
-	var codigoTecla;
-
-	codigoTecla = e.which;
-
-	caracterTecla = String.fromCharCode(codigoTecla);
-	if (caracterTecla == "@") {
-		if (document.registro.correoRegistro.value.indexOf('@') >= 0) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-}
- 
+    if (caracterTecla == "@") {              //Solo una @
+        if (document.encuesta.correo.value.indexOf('@') >= 0) {
+            return false;
+        }else{
+        	return true;
+        }
+    } 
+ }
