e49f49d16b59d0aa25ddc5434363a5c997603115
ikaslea <ikaslea@10.22.72.204>
2017-10-09 13:14:28 +0200

    Registro y envío de mensaje att cliente TERMINADOS

--- Source Files/interfaz/inicio/registro.php
+++ Source Files/interfaz/inicio/registro.php
@@ -11,15 +11,14 @@ $contrasenaComprobacion = $_POST['contrasenaComprobacion'];
 $rol = 'user';
 
 if ($contrasena != $contrasenaComprobacion) {
-    echo '<script>alert("Las contraseñas no coinciden");</script>';
+    echo '<script>alert("Las contraseÃ±as no coinciden");</script>';
     ?>
     <meta http-equiv="refresh" content="5; url=inicio.php">
     <?php
 } else {
     $link = mysqli_connect('localhost', 'root', '', 'ekothazi');
-    
-    $sql = "INSERT INTO 'usuarios' ('nombre', 'apellido', 'email', 'clave', 'rol') VALUES ('$nombre','$apellido','$email','$contrasena','$rol')";
-   mysqli_set_charset($link, "utf8");
+    $sql = "INSERT INTO `usuarios`(`nombre`, `apellido`, `email`, `clave`, `rol`) VALUES ('$nombre','$apellido','$email', '$contrasena', '$rol')";
+    mysqli_set_charset($link, "utf8");
     mysqli_query($link, $sql);
   //  echo '<script>alert("Exito!, tu registro se ha completado.");</script>';
     ?>
