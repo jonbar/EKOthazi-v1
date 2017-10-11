0cef948319ef11ae09a75855f82bf62015c36b8f
ikaslea <ikaslea@10.22.72.204>
2017-10-11 09:28:53 +0200

    Comprobar si el email ya existe en la base de datos

--- Source Files/interfaz/inicio/registro.php
+++ Source Files/interfaz/inicio/registro.php
@@ -9,22 +9,31 @@ $email = $_POST['email'];
 $contrasena = $_POST['contrasena'];
 $contrasenaComprobacion = $_POST['contrasenaComprobacion'];
 $rol = 'user';
+$link = mysqli_connect('localhost', 'root', '', 'ekothazi');
 
 if ($contrasena != $contrasenaComprobacion) {
     echo '<script>alert("Las contraseñas no coinciden");</script>';
     ?>
-    <meta http-equiv="refresh" content="5; url=inicio.php">
+    <meta http-equiv="refresh" content="0; url=inicio.php">
+    <?php
+}
+
+$sql = "SELECT email FROM 'usuarios' WHERE email = '$email'";
+mysqli_set_charset($link, "utf8");
+
+if ((mysqli_query($link, $sql)) >= 1) {
+    echo '<script>alert("El email ya esta registrado");</script>';
+    ?>
+    <meta http-equiv="refresh" content="0; url=inicio.php">
     <?php
 } else {
-    $link = mysqli_connect('localhost', 'root', '', 'ekothazi');
-    $sql = "INSERT INTO `usuarios`(`nombre`, `apellido`, `email`, `clave`, `rol`) VALUES ('$nombre','$apellido','$email', '$contrasena', '$rol')";
+    $sqlDos = "INSERT INTO `usuarios`(`nombre`, `apellido`, `email`, `clave`, `rol`) VALUES ('$nombre','$apellido','$email', '$contrasena', '$rol')";
     mysqli_set_charset($link, "utf8");
-    mysqli_query($link, $sql);
-  //  echo '<script>alert("Exito!, tu registro se ha completado.");</script>';
+    mysqli_query($link, $sqlDos);
+    // echo '<script>alert("Exito!, tu registro se ha completado.");</script>';
     ?>
     <meta http-equiv="refresh" content="5; url=inicio.php">
     <?php
-    
 }
 ?>
 	
