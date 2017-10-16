fb5892a95de8648ba15219fbc688a2855bb9b1c8
barrutieta <barrutieta@10.22.72.121>
2017-10-16 13:14:28 +0200

    falta main

--- Source Files/javascript/main.js
+++ Source Files/javascript/main.js
@@ -91,10 +91,3 @@ function comprobarArroba(e) {
 function alerta(){
 	alert('hola');
 }
-function colorNocturno(){
-	var fichas = document.getElementsByName('ficha');
-	var color = 'black';
-	for (var i = 0; i < fichas.length; i++){
-		f[i].style.background-color = color;
-	}
-}
