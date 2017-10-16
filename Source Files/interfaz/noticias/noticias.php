dfc5dde8f0b00b7c70039dc868205e9e35558fc1
barrutieta <barrutieta@10.22.72.121>
2017-10-16 09:57:52 +0200

    cambiadas algunas cosillas

--- Source Files/interfaz/noticias/noticias.php
+++ Source Files/interfaz/noticias/noticias.php
@@ -107,14 +107,6 @@ session_start();
 			<?php
 
 if ($_SESSION == true) {
-    $link = mysqli_connect('127.0.0.1:51032', 'talde1', 'admin', 'ekothazi');
-    
-    $sql = "SELECT * FROM 'usuarios' WHERE email='" . $_SESSION['username'] . "'";
-    mysqli_set_charset($link, "utf8"); /* Procedural approach */
-    
-    $result = mysqli_query($link, $sql);
-    
-    $row = mysqli_fetch_array($result);
     if ($row['rol'] == 'admin') {
         ?>
 			        <div class="container">
