048c617e7c32ab2d4739b71c5d512f3f8042267f
ikaslea <ikaslea@10.22.72.204>
2017-10-16 13:06:43 +0200

    Cambios query

--- Source Files/interfaz/inicio/registro.php
+++ Source Files/interfaz/inicio/registro.php
@@ -18,11 +18,12 @@ if ($contrasena != $contrasenaComprobacion) {
     <?php
 }
 
-$sql = "SELECT * FROM 'usuarios' WHERE email ='" . $email . "'";
+$sql = "SELECT * FROM 'usuarios' WHERE email='" . $email . "'";
 mysqli_set_charset($link, "utf8");
-$row = mysqli_fetch_assoc($sql);
+$query = mysqli_query($link, $sql);
+$row = mysqli_fetch_assoc($query);
 //ECHOS comprobaciones
-echo $sql;
+echo $sql; 
 echo $row;
 if ($row >= 1) {
     echo '<script>alert("El email ya esta registrado.");</script>';
