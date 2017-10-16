c60a601ba0c79b9b72f2773df0082ecd66415887
barrutieta <barrutieta@10.22.72.121>
2017-10-16 12:52:44 +0200

    fdhgfd

--- Source Files/javascript/noticiasTema.js
+++ Source Files/javascript/noticiasTema.js
@@ -1,8 +1,7 @@
-function cambioNocturno(){
+function colorNocturno(){
 	var fichas = document.getElementsByName('ficha');
 	var color = 'black';
-	body.style.background-color = color;
-//	for (var i = 0; i < ps.length; i++){
-//		f[i].style.background-color = color;
-//	}
+	for (var i = 0; i < fichas.length; i++){
+		f[i].style.background-color = color;
+	}
 }
