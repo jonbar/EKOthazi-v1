7a619354971641d53369129e72defd43f043a836
barrutieta <barrutieta@10.22.72.121>
2017-10-16 10:24:14 +0200

    aaaa

--- Source Files/interfaz/noticias/enviar_noticias.php
+++ Source Files/interfaz/noticias/enviar_noticias.php
@@ -6,7 +6,8 @@ session_start();
 $titulo = $_POST['titulo'];
 $cuerpo = $_POST['textoNoticia'];
 echo $_SESSION['username'] . $titulo . $cuerpo;
-$link = mysqli('127.0.0.1:51032', 'talde1', 'admin', 'ekothazi');
+
+$link = mysqli_connect('127.0.0.1:51032', 'talde1', 'admin', 'ekothazi');
 
 $sql = "SELECT * FROM `usuarios` WHERE email ='" . $_SESSION['username'] . "'";
 mysqli_set_charset($link, "utf8"); /* Procedural approach */
