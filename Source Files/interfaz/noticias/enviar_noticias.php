7f53e493fd0e293e951fbb378c2677a1a0a3e5ed
barrutieta <barrutieta@10.22.72.121>
2017-10-16 10:06:37 +0200

    mas pruebas

--- Source Files/interfaz/noticias/enviar_noticias.php
+++ Source Files/interfaz/noticias/enviar_noticias.php
@@ -6,8 +6,6 @@ session_start();
 $titulo = $_POST['titulo'];
 $cuerpo = $_POST['textoNoticia'];
 
-echo $titulo . $cuerpo;
-
 $link = mysqli('127.0.0.1:51032', 'talde1', 'admin', 'ekothazi');
 
 $sql = "SELECT * FROM `usuarios` WHERE email='" . $_SESSION['username'] . "'";
@@ -16,7 +14,7 @@ mysqli_set_charset($link, "utf8"); /* Procedural approach */
 $result = mysqli_query($link, $sql);
 
 $row = mysqli_fetch_array($result);
-
+echo $sql;
 $id_autor = $row['id'];
 
 $fecha = date("Y-m-d");
