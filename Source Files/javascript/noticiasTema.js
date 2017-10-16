2f14e7aa626c567d0f3c40ad05f0c679e1c0c13b
barrutieta <barrutieta@10.22.72.121>
2017-10-16 13:16:40 +0200

    ddd

--- Source Files/javascript/noticiasTema.js
+++ Source Files/javascript/noticiasTema.js
@@ -1,6 +1,6 @@
 function colorNocturno(){
 	var fichas = document.getElementsByName('ficha');
-	var color = 'black';
+	var color = black;
 	for (var i = 0; i < fichas.length; i++){
 		fichas[i].style.background-color = color;
 	}
