6dfdcec6dac89a4f9770bceb1386145fae5dfdda
barrutieta <barrutieta@10.22.72.121>
2017-10-16 12:14:02 +0200

    alert para comprobacion

--- Source Files/javascript/noticiasTema.js
+++ Source Files/javascript/noticiasTema.js
@@ -1,5 +1,6 @@
 function cambioNocturno(){
 	var fichas = document.getElementsByName('ficha');
+	alert('heey');
 	var color = #0C090A;
 	for (var i = 0; i < ps.length; i++){
 		f[i].style.background-color = color;
