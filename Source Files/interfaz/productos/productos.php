5564f8d60e15fb1fc3a9cef8990ce6100e6723fa
barrutieta <barrutieta@10.22.72.121>
2017-10-17 13:07:11 +0200

    a tooope

--- Source Files/interfaz/productos/productos.php
+++ Source Files/interfaz/productos/productos.php
@@ -117,7 +117,7 @@ session_start();
 			<div class="container-fluid">
 				<div class="row" style="background-color: #E6E6E6;">
 					<div class="col-sm-4">
-						<img src="../../img/logoLargoFooter.png" class="imagenFooter imgLogoFooter">
+						<img src="../../img/logoLargoFooter.png" class="imgLogoFooter">
 					</div>
 					<div class="col-sm-3">
 						<h4 style="color: 7ac043;">Información</h4>
