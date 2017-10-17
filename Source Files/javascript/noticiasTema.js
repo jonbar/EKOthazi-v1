738bfa00a8e266fd768a5187a0b706946eba72be
barrutieta <barrutieta@10.22.72.121>
2017-10-17 12:12:45 +0200

    mas pruebas...

--- Source Files/javascript/noticiasTema.js
+++ Source Files/javascript/noticiasTema.js
@@ -8,11 +8,10 @@ function colorNocturno(){
 		for (var i = 0; i < fichas.length; i++){
 			fichas[i].style.backgroundColor = 'black';
 			body.style.backgroundColor = 'gray';
+			fichas[i].style.color = 'white';
 			divBotones.style.backgroundColor = 'gray';
 			divBotonAdmin.style.backgroundColor = 'gray';
 			divBotonUser.style.backgroundColor = 'gray';
-			
-			fichas[i].style.color = 'white';
 		}
 	}
 function colorNormal(){
@@ -22,12 +21,12 @@ function colorNormal(){
 	var divBotonAdmin =  document.getElementById('divBotonAdmin');
 	var divBotonUser =  document.getElementById('divBotonUser');
 	
-	for (var i = 0; i < fichas.length; i++){
+	for (var j = 0; j < fichas.length; j++){
 		body.style.backgroundColor = 'white';
-		divBotones.style.backgroundColor = 'white';
-		divBotonAdmin.style.backgroundColor = 'white';
-		divBotonUser.style.backgroundColor = 'white';
 		fichas[i].style.backgroundColor = 'white';
 		fichas[i].style.color = 'black';
+		divBotones.style.backgroundColor = 'white';
+		divBotonAdmin.style.backgroundColor = 'white';
+		divBotonUser.style.backgroundColor = 'white';	
 	}
 }
