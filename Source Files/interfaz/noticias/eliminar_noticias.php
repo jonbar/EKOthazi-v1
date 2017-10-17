f23f27f3444947973c2b5c666dd928e7928031a2
ikaslea <ikaslea@10.22.72.204>
2017-10-17 13:33:54 +0200

    Comentarios en apartado noticias

--- Source Files/interfaz/noticias/eliminar_noticias.php
+++ Source Files/interfaz/noticias/eliminar_noticias.php
@@ -1,15 +1,20 @@
 <?php
 $link = mysqli_connect('127.0.0.1:51032', 'talde1', 'admin', 'ekothazi');
+/*
+ * Se recoge ID de entrada seleccionada en combo (noticias.php)
+ */
 
 $id_entrada = $_POST['comboEntrada'];
-echo $id_entrada;
 
+/*
+ * Se elimina de la BBDD entrada con esa ID
+ */
 $sql = "DELETE FROM `entrada_blog` WHERE id_entrada = '$id_entrada'";
 mysqli_set_charset($link, "utf8"); /* Procedural approach */
 
 $result = mysqli_query($link, $sql);
 ?>
-
+<html>
 <head>
 <meta http-equiv="refresh" content="0; url=noticias.php"/>
 </head>
--- Source Files/interfaz/noticias/enviar_noticias.php
+++ Source Files/interfaz/noticias/enviar_noticias.php
@@ -3,22 +3,29 @@ session_start();
 ?>
 
 <?php
+
+/*
+ * Se recogen datos de crear entrada
+ */
 $titulo = $_POST['titulo'];
 $cuerpo = $_POST['textoNoticia'];
 
+/*
+ * Se recoge datos de usuario con sesión iniciada
+ */
 $link = mysqli_connect('127.0.0.1:51032', 'talde1', 'admin', 'ekothazi');
-
 $sql = "SELECT * FROM `usuarios` WHERE email ='" . $_SESSION['username'] . "'";
-mysqli_set_charset($link, "utf8"); /* Procedural approach */
 
+mysqli_set_charset($link, "utf8"); /* Procedural approach */
 $result = mysqli_query($link, $sql);
 
 $row = mysqli_fetch_array($result);
-
 $id_autor = $row['id'];
-
 $fecha = date("Y-m-d");
 
+/*
+ * Introducción de entrada en la BBDD
+ */
 $sqlDos="INSERT INTO `entrada_blog` (`id_autor`, `titulo`, `cuerpo`, `fecha`, `imagen1`) 
 VALUES ('$id_autor','$titulo','$cuerpo','$fecha', 'NULL')";
 mysqli_query($link, $sqlDos);
