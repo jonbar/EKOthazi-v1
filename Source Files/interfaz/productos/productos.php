5bb6cf4e3a381313942d0e87418099330859e59a
ikaslea <ikaslea@10.22.72.204>
2017-10-18 12:55:53 +0200

    main.js importado

--- Source Files/interfaz/productos/productos.php
+++ Source Files/interfaz/productos/productos.php
@@ -15,6 +15,7 @@ session_start();
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="../../javascript/main.js"></script>
 
 </head>
 <body>
